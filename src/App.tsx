40a7ba8699927500eb478e663b1691285e602c23
huangnan <huangnan@huya.com>
2024-11-08 17:39:34 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React,{ useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+     app
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root') as Element).render(
+    <App />
+)
